cffd0f33ccc0e3309e11f6c2d449636cd6fb788f
elicate <gabrielkasonde246@gmail.com>
2023-06-25 22:20:00 +0200

    Update openai.tsx

--- app/src/global-options/openai.tsx
+++ app/src/global-options/openai.tsx
@@ -1,23 +1,21 @@
+import { FormattedMessage } from "react-intl";
 import { OptionGroup } from "../core/options/option-group";
-import dotenv from 'dotenv';
-import path from 'path';
-
-dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 export const openAIOptions: OptionGroup = {
-  id: 'openai',
-  apiKey: process.env.OPENAI_API_KEY,
-  options: [
-    {
-      id: 'apiKey',
-      displayOnSettingsScreen: 'user',
-      displayAsSeparateSection: false,
-      renderProps: () => ({
-        type: 'password',
-        label: 'Your OpenAI API Key',
-        placeholder: 'sk-************************************************',
-        description: <></>,
-      }),
-    },
-  ],
-};
+    id: 'openai',
+    options: [
+        {
+            id: 'apiKey',
+            defaultValue: "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",
+            displayOnSettingsScreen: "user",
+            displayAsSeparateSection: false,
+            renderProps: () => ({
+                type: "password",
+                label: "Your OpenAI API Key",
+                placeholder: "sk-************************************************",
+                description: <>
+                </>,
+            }),
+        },
+    ],
+}
