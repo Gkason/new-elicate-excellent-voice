b15cd292e464df5c69337c7b14d4f238be6d5f8d
elicate <gabrielkasonde246@gmail.com>
2023-06-25 19:45:24 +0200

    Update openai.tsx

--- app/src/global-options/openai.tsx
+++ app/src/global-options/openai.tsx
@@ -1,3 +1,4 @@
+import { FormattedMessage } from "react-intl";
 import { OptionGroup } from "../core/options/option-group";
 
 export const openAIOptions: OptionGroup = {
@@ -8,8 +9,25 @@ export const openAIOptions: OptionGroup = {
             defaultValue: "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",
             displayOnSettingsScreen: "user",
             displayAsSeparateSection: true,
-            renderProps: () => null, // Returns null instead of an object for rendering the input field
+            renderProps: () => ({
+                type: "password",
+                label: "Your OpenAI API Key",
+                placeholder: "sk-************************************************",
+                description: <>
+                    <p>
+                        <a href="https://platform.openai.com/account/api-keys" target="_blank" rel="noreferrer">
+                            <FormattedMessage defaultMessage="Find your API key here." description="Label for the link that takes the user to the page on the OpenAI website where they can find their API key." />
+                        </a>
+                    </p>
+                    <p>
+                        <FormattedMessage defaultMessage="Your API key is stored only on this device and never transmitted to anyone except OpenAI." />
+                    </p>
+                    <p>
+                        <FormattedMessage defaultMessage="OpenAI API key usage is billed at a pay-as-you-go rate, separate from your ChatGPT subscription." />
+                    </p>
+                </>,
+                disabled: true, // Disables the input field for the API key option
+            }),
         },
     ],
 };
-
